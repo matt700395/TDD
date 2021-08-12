d69e64b3c2ea54f74d73819117b5a47e6854edc5
matt700395 <matt7003@korea.ac.kr>
2021-08-12 16:13:50 +0900

    test views

--- firstapp/tests.py
+++ firstapp/tests.py
@@ -1,7 +1,19 @@
+#firstapp/tests.py
+from django.http import HttpRequest
 from django.test import TestCase
+from django.urls import resolve
+from firstapp.views import home_page
 
-# Create your tests here.
-class SmokeTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))#3
+        self.assertIn(b'<title>To-Do lists</title>', response.content) #4
+        self.assertTrue(response.content.endwith(b'</html>'))#5
