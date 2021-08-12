cd9cae2a666dc9b227f9bf8950b27d95319d0dd4
matt700395 <matt7003@korea.ac.kr>
2021-08-12 10:51:43 +0900

    chap3_1

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,24 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Chrome('chromedriver.exe')
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):#1
 
-assert 'Django' in browser.title
+    def setUp(self): #2
+        self.browser = webdriver.Chrome('./chromedriver')
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):#3
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self): #4
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)#5
+        self.fail('Finish the test!')#6
+
+
+if __name__ == '__main__': #7
+    unittest.main(warnings='ignore') #8
+
+
+
