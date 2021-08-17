d126a569debdee145cd2561a55e27695104a54a6
matt700395 <matt7003@korea.ac.kr>
2021-08-17 11:47:59 +0900

    5_1 post요청을 전송하기 위한 Form 연동

--- functional_test.py
+++ functional_test.py
@@ -2,6 +2,7 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 class NewVisitorTest(unittest.TestCase):#1
 
@@ -24,19 +25,28 @@ class NewVisitorTest(unittest.TestCase):#1
         #그녀는 바로 작업을 추가하기로 한다
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            inputbox.get_attribute('placeholer'),
+            inputbox.get_attribute('placeholder'),
+            '작업 아이템 입력'
                          )
 
         #공작깃털 사기라고 텍스트 상자에 입력한다
         #에디스의 취미는 날치잡이용 그물을 만드는 것이다
+        inputbox.send_keys('공작깃털 사기')
+
+
         inputbox.send_keys(Keys.ENTER)
 
+        time.sleep(10)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1:공작깃털사기' for row in rows),
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows),
+            "신규 작업이 테이블에 표시되지 않는다"
                         )
 
         self.fail("Finish the test!")
+
+        #browser.quit()
 
 if __name__ == '__main__': #7
     unittest.main(warnings='ignore') #8
